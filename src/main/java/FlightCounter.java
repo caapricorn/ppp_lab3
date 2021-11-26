1bb8fa0778bfbf3a11d1ed9ea60e3f93b6b2ba75
caapricorn <caapricorn@mail.ru>
2021-11-27 01:54:54 +0300

    Scripted auto-commit on change (2021-11-27 01:54:54) by gitwatch.sh

--- src/main/java/FlightCounter.java
+++ src/main/java/FlightCounter.java
@@ -1,2 +1,11 @@
-public class FlightCounter {
+import java.io.Serializable;
+
+public class FlightCounter implements Serializable {
+
+
+    public FlightCounter() {
+    }
+
+    public FlightCounter() {
+    }
 }
