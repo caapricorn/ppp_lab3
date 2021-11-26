eff76830d674ccb0120d3f74d3d52881d4f0d754
caapricorn <caapricorn@mail.ru>
2021-11-27 02:05:09 +0300

    Scripted auto-commit on change (2021-11-27 02:05:09) by gitwatch.sh

--- src/main/java/FlightCounter.java
+++ src/main/java/FlightCounter.java
@@ -35,4 +35,8 @@ public class FlightCounter implements Serializable {
     public float getMaxDelay() {
         return maxDelay;
     }
+
+    public int getDelayFlights() {
+        return delayFlights;
+    }
 }
