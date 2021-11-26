296468b67536d21750c17017111bb12a4f6be9e3
caapricorn <caapricorn@mail.ru>
2021-11-27 02:01:31 +0300

    Scripted auto-commit on change (2021-11-27 02:01:31) by gitwatch.sh

--- src/main/java/FlightCounter.java
+++ src/main/java/FlightCounter.java
@@ -1,11 +1,28 @@
 import java.io.Serializable;
 
 public class FlightCounter implements Serializable {
-
+    private float maxDelay;
+    private int delayFlights;
+    private int cancelledFlights;
+    private int flightCount;
 
     public FlightCounter() {
     }
 
-    public FlightCounter(float maxDelay) {
+    public FlightCounter(float maxDelay, int delayFlights, int cancelledFlights, int flightCount) {
+        this.maxDelay = maxDelay;
+        this.delayFlights = delayFlights;
+        this.cancelledFlights = cancelledFlights;
+        this.flightCount = flightCount;
     }
+
+    public void setMaxDelay(int maxDelay) {
+        this.maxDelay = maxDelay;
+    }
+
+    public void setDelayFlights(int delayFlights) {
+        this.delayFlights = delayFlights;
+    }
+
+    
 }
