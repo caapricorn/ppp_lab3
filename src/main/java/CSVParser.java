489c94df01f4edabd1065618ebe6b23bd9273f7d
caapricorn <caapricorn@mail.ru>
2021-11-27 02:56:27 +0300

    Scripted auto-commit on change (2021-11-27 02:56:27) by gitwatch.sh

--- src/main/java/CSVParser.java
+++ src/main/java/CSVParser.java
@@ -1,8 +1,11 @@
 import org.apache.spark.api.java.JavaRDD;
 
 public class CSVParser {
-    public static JavaRDD<String> parseAirport() {
-
+    public static JavaRDD<String> parseAirport(JavaRDD<String> distFile) {
+        distFile = distFile
+                .filter(s -> !s.contains("Code"))
+                .map(s -> s=s.replace("\"", ""));
+        return distFile;
     }
 
     public static JavaRDD<String> parseFlight() {
