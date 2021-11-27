cc32a9c62dd98f69556826b997deeb0a2fd7fe09
caapricorn <caapricorn@mail.ru>
2021-11-27 03:20:31 +0300

    Scripted auto-commit on change (2021-11-27 03:20:31) by gitwatch.sh

--- src/main/java/FlightStatisticsApp.java
+++ src/main/java/FlightStatisticsApp.java
@@ -6,7 +6,7 @@ public class FlightStatisticsApp {
 
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
-            System.err.println("Usage: ");
+            System.err.println("Usage: <input path Airport> <input path Flight> <output path>");
         }
     }
 }
