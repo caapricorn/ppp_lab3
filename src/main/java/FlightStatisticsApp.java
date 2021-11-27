f6356d7cfc930c42a65935ac8592090dd5caff4c
caapricorn <caapricorn@mail.ru>
2021-11-27 03:20:23 +0300

    Scripted auto-commit on change (2021-11-27 03:20:23) by gitwatch.sh

--- src/main/java/FlightStatisticsApp.java
+++ src/main/java/FlightStatisticsApp.java
@@ -1,6 +1,12 @@
 public class FlightStatisticsApp {
-    public static final int ORIGIN_AIRPORT_ID = 11;
-    public static final int DEST_AIRPORT_ID = 14;
-    public static final int DELAY_TIME = 18;
-    
+    private static final int ORIGIN_AIRPORT_ID = 11;
+    private static final int DEST_AIRPORT_ID = 14;
+    private static final int DELAY_TIME = 18;
+    private static final int IS_CANCELLED = 19;
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.err.println("Usage: ");
+        }
+    }
 }
