3cac08a3ff413f820ad85be253c1c634affc205e
caapricorn <caapricorn@mail.ru>
2021-11-27 04:39:21 +0300

    Scripted auto-commit on change (2021-11-27 04:39:21) by gitwatch.sh

--- src/main/java/FlightStatisticsApp.java
+++ src/main/java/FlightStatisticsApp.java
@@ -87,6 +87,6 @@ public class FlightStatisticsApp {
                     return originAirportName + "===>" + destAirportName + "\n" + value;
                 });
 
-        resultOutput.saveAsTextFile("/home/caapricorn/output");
+        resultOutput.saveAsTextFile("hdfs://localhost:9000/home/caapricorn/output");
     }
 }
