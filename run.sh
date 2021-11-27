33db3edc23f5343c946fb1e9bdd5eaa9eba8cd88
caapricorn <caapricorn@mail.ru>
2021-11-27 04:47:11 +0300

    Scripted auto-commit on change (2021-11-27 04:47:11) by gitwatch.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 mvn package
 hadoop fs -rm -f -r output
+export HADOOP_CLASSPATH=./target/hadoop-examples-1.0-SNAPSHOT.jar
 spark-submit --class FlightStatisticsApp --master yarn-client --num-executors 3 ./target/spark-examples-1.0-SNAPSHOT.jar 
 rm -rf output
 hadoop fs -copyToLocal output
