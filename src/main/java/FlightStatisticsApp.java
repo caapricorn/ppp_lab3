ef27d4c759bdab70bce06321b8222bb1532732cc
caapricorn <caapricorn@mail.ru>
2021-12-03 23:38:27 +0300

    Scripted auto-commit on change (2021-12-03 23:38:27) by gitwatch.sh

--- src/main/java/FlightStatisticsApp.java
+++ src/main/java/FlightStatisticsApp.java
@@ -13,7 +13,7 @@ public class FlightStatisticsApp {
     private static final int DELAY_TIME = 18;
     private static final int IS_CANCELLED = 19;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         if (args.length != 3) {
             System.err.println("Usage: <input path Airport> <input path Flight> <output path>");
             System.exit(-1);
